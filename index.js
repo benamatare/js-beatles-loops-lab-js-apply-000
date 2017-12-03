d0235f898c61032911ea683ebeafeeab600bc6b9
Ben Matare <ben.a.matare@gmail.com>
2017-12-03 19:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ describe('Beatles Loops', function() {
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
 */
-
+// This says that this function looks at musicians and instruments (two arrays)
+// and creates a new array 
+// then it iterates through each array 
+// then combines the indexes and the text plays
+// into the new array as musicians[1] + "plays" instruments[1]
 function theBeatlesPlay(musicians, instruments) {
   var combinedStuff = [];
-
   for (var i=0; i < musicians.length; i++) {
     combinedStuff.push(musicians[i] += " plays " + instruments[i]);
   }
   return combinedStuff;
+}
+
+
+function johnLennonFacts(facts) {
+  newArr = [];
+  var i = 0;
+  
+  while (i < facts.length) {
+    newArr.push(facts[i] +"!!!");
+    i++;
+  }
+  return newArr;
 }
